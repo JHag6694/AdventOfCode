c267d7422b6b784ecf21309afbc71b521e812da6
Jérôme H <j.haguet@gmail.com>
2021-11-11 10:28:06 +0100

    first commit

---
+++ aoc1.py
@@ -0,0 +1,24 @@
+#
+import os
+
+def log(text):
+    print(text)
+
+
+dataFile=os.getcwd()+"\\aoc-1.txt"
+items = []
+f = open(dataFile, "r")
+for x in f:
+    # log("line:".format(x))
+    items.append(int(x))
+f.close()
+
+for i in range(0, len(items)):
+    for j in range(i+1, len(items)):
+        sum = items[i]+items[j]
+        #log("Sum({0}, {1})={2}".format(i, j, sum))
+        if (sum == 2020):
+            prod = items[i]*items[j]
+            log("Found : Index=(i={}, j={}); values={},{}; Prod={}".format(
+                i, j, items[i], items[j], prod))
+
