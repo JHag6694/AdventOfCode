49a288f42ba14c7bf63e7c4f051a58bd2eb83986
JHag6694 <j.haguet@gmail.com>
2021-12-02 18:33:17 +0100

    Puzzle 2 / part 2

--- p02.py
+++ p02.py
@@ -26,3 +26,18 @@ for i, item in enumerate(items):
         y_depth = y_depth - int(item[2:])
 
 log("x_pos={}; depth={}; prod={}".format(x_pos, y_depth, x_pos*y_depth))
+
+# Part 2
+x_pos = 0
+y_depth = 0
+z_aim=0
+for i, item in enumerate(items):
+    if (item.startswith("forward")):
+        x_pos = x_pos + int(item[7:])
+        y_depth=y_depth+z_aim*int(item[7:])
+    elif (item.startswith("down")):
+        z_aim = z_aim + int(item[4:])
+    elif (item.startswith("up")):
+        z_aim = z_aim - int(item[2:])
+
+log("x_pos={}; depth={}; prod={}".format(x_pos, y_depth, x_pos*y_depth))
