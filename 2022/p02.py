302fe756eb1cf717d94ec8d8e2e4d5acc7eaa764
JHag6694 <j.haguet@gmail.com>
2024-01-09 21:17:12 +0100

    p02-p1

--- 2022/p02.py
+++ 2022/p02.py
@@ -1,14 +1,28 @@
 #
 import os
 
+
 def log(text):
     print(text)
 
 
 file_path = os.getcwd() + "\\2022\\p02-input.txt"
-items = []
 with open(file_path, "r") as file:
     lines = [line.strip() for line in file.readlines()]
 
 
 # Stage 1 :
+score = 0
+for i in range(len(lines)):
+    oppPlay = ord(lines[i][0]) - ord("A") + 1
+    myPlay = ord(lines[i][-1]) - ord("X") + 1
+    diff = (myPlay - oppPlay) % 3
+    if diff == 0:
+        res = 3
+    elif diff == 1:
+        res = 6
+    else:
+        res = 0
+    score += myPlay + res
+
+log("Score : {}".format(score))
