f8113dfbd7d9ed45600cba459a8cfc1e7b4d3815
JHag6694 <j.haguet@gmail.com>
2024-01-09 21:49:49 +0100

    Comments

--- 2022/p02.py
+++ 2022/p02.py
@@ -10,13 +10,15 @@ file_path = os.getcwd() + "\\2022\\p02-input.txt"
 with open(file_path, "r") as file:
     lines = [line.strip() for line in file.readlines()]
 
-# Rock : 1, Paper : 2, Scissors : 3
+# Paly : Rock:1, Paper:2, Scissors:3
+# Result : 0:lost, 1:draw, 2:won     
+
 # Stage 1 :
 score = 0
 for i in range(len(lines)):
-    oppPlay = ord(lines[i][0]) - ord("A") + 1  # 1, 2, 3
-    myPlay = ord(lines[i][-1]) - ord("X") + 1  # 1, 2, 3
-    res = (myPlay - oppPlay + 1) % 3  # 0 lost, 1 :draw, 2: won
+    oppPlay = ord(lines[i][0]) - ord("A") + 1  #Rock:1, Paper:2, Scissors:3
+    myPlay = ord(lines[i][-1]) - ord("X") + 1  #Rock:1, Paper:2, Scissors:3
+    res = (myPlay - oppPlay + 1) % 3  # 0:lost, 1:draw, 2:won
     score = score + myPlay + (res * 3)
 
 log("Score1 : {}".format(score))  # 13924
@@ -24,9 +26,9 @@ log("Score1 : {}".format(score))  # 13924
 # Stage 2 :
 score2 = 0
 for i in range(len(lines)):
-    oppPlay = ord(lines[i][0]) - ord("A") + 1  # 1, 2 or 3
+    oppPlay = ord(lines[i][0]) - ord("A") + 1  #Rock:1, Paper:2, Scissors:3
     res = ord(lines[i][-1]) - ord("X")  # 0:lost 1:draw 2:won
     myPlay = ((oppPlay + res - 1) - 1) % 3 + 1
     score2 += myPlay + (res * 3)
 
-log("Score2 : {}".format(score2))
+log("Score2 : {}".format(score2)) # 13448
